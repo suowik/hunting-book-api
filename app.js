b61dc22a67c9952e2484d60bb4514369095e9688
Paweł Słowik <pawel.slowik@schibsted.pl>
2017-07-19 14:42:44 +0200

    proper mongo URI variable

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let app = require('./src/routes.js').app;
 
-let dbURI = process.env.MONGO_URI || 'mongodb://192.168.99.100:27017/hb2';
+let dbURI = process.env.MONGODB_URI || 'mongodb://192.168.99.100:27017/hb2';
 let MongoClient = require('mongodb').MongoClient;
 let connection = MongoClient.connect(dbURI);
 
