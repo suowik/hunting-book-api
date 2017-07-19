2d58c48b227c9264fd7f7734fd0a04080446117b
Paweł Słowik <pawel.slowik@schibsted.pl>
2017-07-19 10:57:03 +0200

    endpoints refactor

--- src/users/endpoints.js
+++ src/users/endpoints.js
@@ -1,25 +1,24 @@
 let endpoints = (router, repository) => {
-    router.route('/')
-        .get((req, res) => {
-            repository.findAll({limit: parseInt(req.query.limit) || 5, offset: parseInt(req.query.offset) || 0})
-                .then(users => {
-                    res.json(users);
-                })
-                .catch(err => {
-                    res.send(err)
-                })
-        })
-        .post((req, res) => {
-            let user = req.body;
-            repository.create(user)
-                .then(() => {
-                    res.json(user)
-                })
-                .catch((err) => {
-                    console.log(err);
-                    res.sendStatus(409);
-                });
-        });
+    router.get('/', (req, res) => {
+        repository.findAll({limit: parseInt(req.query.limit) || 5, offset: parseInt(req.query.offset) || 0})
+            .then(users => {
+                res.json(users);
+            })
+            .catch(err => {
+                res.send(err)
+            })
+    });
+    router.post('/', (req, res) => {
+        let user = req.body;
+        repository.create(user)
+            .then(() => {
+                res.json(user)
+            })
+            .catch((err) => {
+                console.log(err);
+                res.sendStatus(409);
+            });
+    });
     return router
 };
 
