731aa7cb6a50a3de0bae675b43a7046ff7226fef
Paweł Słowik <pawel.slowik@schibsted.pl>
2017-08-25 13:25:08 +0200

    scheduled job for hunting status change

--- app.js
+++ app.js
@@ -3,19 +3,27 @@ let app = require('./src/routes.js').app;
 let dbURI = process.env.MONGODB_URI || 'mongodb://heroku_p736np9g:68n864jmg3nkjrlht3s0eg599m@ds161022.mlab.com:61022/heroku_p736np9g';
 let MongoClient = require('mongodb').MongoClient;
 let connection = MongoClient.connect(dbURI);
-
+let findAndFinishHuntingsJob = require('./src/schedule/findAndFinishHuntings.js').findAndFinishHuntingsJob;
 
 let UserRepository = require('./src/users/UserRepository.js').UserRepository;
 let AnimalRepository = require('./src/animals/AnimalRepository.js').AnimalRepository;
 let HuntingRepository = require('./src/huntings/HuntingRepository.js').HuntingRepository;
 let HuntingAreasRespository = require('./src/huntingAreas/HuntingAreaRepository.js').HuntingAreaRepository;
 
+let huntingRepository = new HuntingRepository(connection);
+
+findAndFinishHuntingsJob(huntingRepository)();
+
 let repositories = {
     userRepository: new UserRepository(connection),
     animalRepository: new AnimalRepository(connection),
-    huntingRepository: new HuntingRepository(connection),
+    huntingRepository: huntingRepository,
     huntingAreaRepository: new HuntingAreasRespository(connection)
 };
 
+
 app(repositories);
+
+
+
 
---
+++ src/schedule/findAndFinishHuntings.js
@@ -0,0 +1,30 @@
+let schedule = require('node-schedule');
+let moment = require('moment');
+
+
+function findAndFinishHuntingsJob(huntingRepository) {
+    return () => {
+        schedule.scheduleJob("* * * * * *", () => {
+            let now = moment();
+            huntingRepository.findAll(null, {status:'started'})
+                .then((huntings) => {
+                    huntings.forEach(hunting => {
+                        let endOfHunting = moment(hunting.end);
+                        if (now.isAfter(endOfHunting)) {
+                            huntingRepository.finish({_id: hunting._id, end: hunting.end})
+                        }
+
+                    })
+                })
+                .catch(e => {
+                    console.log(e)
+                });
+        });
+    }
+}
+
+module.exports = {
+    findAndFinishHuntingsJob: findAndFinishHuntingsJob
+};
+
+
