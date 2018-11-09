87d2af742d543a5cc9f8ef9328115e43fcbe6e1f
pawelslowik <pawel.slowik@schibsted.pl>
2018-11-09 10:56:04 +0100

    announcements

--- src/announcements/AnnouncementRepository.js
+++ src/announcements/AnnouncementRepository.js
@@ -9,15 +9,40 @@ class AnnouncementRepository extends CRUD {
             keyUniqueness: (entity) => {
                 return {_id: entity._id}
             },
-            beforeInsert: (announcement)=>{
+            beforeInsert: (announcement) => {
                 if (announcement._id !== null) {
                     announcement._id = new mDB.ObjectID(announcement._id)
                 }
                 if (announcement._id === null || announcement._id === undefined) {
                     announcement._id = new mDB.ObjectID()
                 }
+                announcement.userId = new mDB.ObjectID(announcement.userId);
                 return announcement
-            }
+            },
+            afterFind: (announcement) => {
+                return {
+                    "user": {
+                        name: announcement.user[0].name,
+                        surname: announcement.user[0].surname
+                    },
+                    "_id": announcement._id,
+                    "content": announcement.content,
+                    "announced": announcement.announced
+                }
+            },
+            lookupQuery: async (collection, criteria) => {
+                return await collection.aggregate([
+                    {$match: criteria},
+                    {
+                        "$lookup": {
+                            "from": "users",
+                            "localField": "userId",
+                            "foreignField": "_id",
+                            "as": "user"
+                        }
+                    }
+                ])
+            },
         })
     }
 }
